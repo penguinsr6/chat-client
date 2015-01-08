80f1e03c296431c5a598633d20bf8422c51c3163
penguinsr6 <rsikdar@berkeley.edu>
2015-01-08 02:15:35 -0500

    added people in room to friend's list

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ app.use(session({
 	secret: 'qwerty',
 
 }));
+var all_users = [];
 // var tmp;
 app.get('/', function(req, res) {
 	sess = req.session;
 	sess.user = 'unknown';
 	res.sendFile(__dirname + '/index.html');
 });
+
+// app.get('/login', function(req, res) {
+// 	res.render(__dirname + '/login.html');
+// });
 
 io.on('connection', function(socket){
 	console.log('user connected');
@@ -23,10 +28,14 @@ io.on('connection', function(socket){
 	socket.on('connected', function(msg){
 		var req = socket.request;
 		req.user = msg[0];
-		io.emit('connected', msg[1]);
+		all_users.push(msg[0]);
+		io.emit('connected', msg[1], all_users);
 	});
 	socket.on('disconnect', function(){
-		io.emit('disconnect', socket.request.user + ' has disconnected');
+		var index = all_users.indexOf(socket.request.user);
+		all_users.splice(index, 1);
+		console.log(all_users);
+		io.emit('disconnect', socket.request.user + ' has disconnected', all_users);
 		console.log('user disconnected');
 	});
 });
