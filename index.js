80c4271a8c7d6f7a1e12bb3ed3229cee2336f77c
penguinsr6 <rsikdar@berkeley.edu>
2015-01-22 10:34:07 -0800

    created user typing feature

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ app.use(session({
 
 }));
 var all_users = [];
+var users_typing = [];
 // var tmp;
 app.get('/', function(req, res) {
 	sess = req.session;
 	sess.user = 'unknown';
 	res.sendFile(__dirname + '/index.html');
 });
-
-// app.get('/login', function(req, res) {
-// 	res.render(__dirname + '/login.html');
-// });
 
 io.on('connection', function(socket){
 	console.log('user connected');
@@ -32,12 +29,36 @@ io.on('connection', function(socket){
 		io.emit('connected', msg[1], all_users);
 	});
 	socket.on('disconnect', function(){
-		var index = all_users.indexOf(socket.request.user);
-		all_users.splice(index, 1);
+		var index1 = all_users.indexOf(socket.request.user);
+		var index2 = users_typing.indexOf(socket.request.user);
+		all_users.splice(index1, 1);
+		users_typing.splice(index2, 1);
 		console.log(all_users);
 		io.emit('disconnect', socket.request.user + ' has disconnected', all_users);
+		io.emit('typing', users_typing);
 		console.log('user disconnected');
 	});
+	socket.on('typing', function(user){
+		console.log('current', users_typing);
+		if (users_typing.indexOf(user) < 0) {
+			console.log('adding user', user);
+			users_typing.push(user);
+			console.log('added', users_typing);
+			io.emit('typing', users_typing);
+		}
+	});
+	socket.on('typing_remove', function(user){
+		console.log('current -to remove', user, users_typing);
+		console.log(users_typing.indexOf(user));
+		var index = users_typing.indexOf(user);
+		if (index >= 0) {
+			console.log('removing user', user);
+			users_typing.splice(index, 1);
+			console.log('removed', users_typing);
+			io.emit('typing', users_typing);
+		}
+	});
+
 });
 
 
