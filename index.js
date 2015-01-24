e95683912bfd47458a88fa880ca2cb19635df513
penguinsr6 <rsikdar@berkeley.edu>
2015-01-23 20:18:32 -0800

    cleaned up and condensed code

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(session({
 var all_users = [];
 var users_typing = [];
 var users_text_entered = [];
-// var tmp;
 app.get('/', function(req, res) {
 	sess = req.session;
 	sess.user = 'unknown';
@@ -19,7 +18,6 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
-	// console.log('user connected');
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
@@ -27,8 +25,9 @@ io.on('connection', function(socket){
 		var req = socket.request;
 		req.user = msg[0];
 		all_users.push(msg[0]);
-		io.emit('connected', msg[1], all_users);
+		io.emit('connect and disconnect', msg[1], all_users);
 	});
+
 	socket.on('disconnect', function(){
 		var index1 = all_users.indexOf(socket.request.user);
 		var index2 = users_typing.indexOf(socket.request.user);
@@ -36,58 +35,45 @@ io.on('connection', function(socket){
 		all_users.splice(index1, 1);
 		users_typing.splice(index2, 1);
 		users_text_entered.splice(index3, 1);
-		// console.log(all_users);
-		io.emit('disconnect', socket.request.user + ' has disconnected', all_users);
-		io.emit('typing', users_typing);
-		io.emit('text_entered', users_text_entered);
-		// console.log('user disconnected');
+		io.emit('connect and disconnect', socket.request.user + ' has disconnected', all_users);
+		io.emit('text_entered', users_text_entered, users_typing);
 	});
+
 	socket.on('typing', function(user){
-		// console.log('current', users_typing);
 		if (users_typing.indexOf(user) < 0) {
-			// console.log('adding user', user);
 			users_typing.push(user);
-			// console.log('added', users_typing);
 			io.emit('typing', users_typing);
 		}
 	});
+
 	socket.on('text_entered', function(user){
-		// console.log(user);
-		// console.log('text entered', users_text_entered);
-		// console.log(users_typing.indexOf(user));
 		if (users_text_entered.indexOf(user) < 0) {
-			// console.log('adding user to text enter', user);
 			users_text_entered.push(user);
-			// console.log('added to text enter', users_text_entered);
-			io.emit('text_entered', users_text_entered);
+			var index = users_typing.indexOf(user);
+			if (index >= 0) {
+				users_typing.splice(index, 1);
+			}
+			io.emit('text_entered', users_text_entered, users_typing);
 		}
 	});
+
 	socket.on('text_entered_remove', function(user){
-		// console.log('text enter to remove', user, users_text_entered);
-		// console.log(users_text_entered.indexOf(user));
 		var index = users_text_entered.indexOf(user);
 		if (index >= 0) {
-			// console.log('removing user', user);
 			users_text_entered.splice(index, 1);
-			// console.log('removed', users_text_entered);
-			io.emit('text_entered', users_text_entered);
+			io.emit('text_entered', users_text_entered,users_typing);
 		}
 	});
+
 	socket.on('typing_remove', function(user){
-		// console.log('current -to remove', user, users_typing);
-		// console.log(users_typing.indexOf(user));
 		var index = users_typing.indexOf(user);
 		if (index >= 0) {
-			// console.log('removing user', user);
 			users_typing.splice(index, 1);
-			// console.log('removed', users_typing);
 			io.emit('typing', users_typing);
 		}
 	});
 
 });
-
-
 
 http.listen(3000, function(){
 	console.log('listening on port 3000');
