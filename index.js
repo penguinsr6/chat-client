e62c9ebf24f80c8012460cd77ad088d73d9e12e7
penguinsr6 <rsikdar@berkeley.edu>
2015-01-23 18:47:06 -0800

    added a text entered

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(session({
 }));
 var all_users = [];
 var users_typing = [];
+var users_text_entered = [];
 // var tmp;
 app.get('/', function(req, res) {
 	sess = req.session;
@@ -18,7 +19,7 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
-	console.log('user connected');
+	// console.log('user connected');
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
@@ -31,30 +32,55 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		var index1 = all_users.indexOf(socket.request.user);
 		var index2 = users_typing.indexOf(socket.request.user);
+		var index3 = users_text_entered.indexOf(socket.request.user);
 		all_users.splice(index1, 1);
 		users_typing.splice(index2, 1);
-		console.log(all_users);
+		users_text_entered.splice(index3, 1);
+		// console.log(all_users);
 		io.emit('disconnect', socket.request.user + ' has disconnected', all_users);
 		io.emit('typing', users_typing);
-		console.log('user disconnected');
+		io.emit('text_entered', users_text_entered);
+		// console.log('user disconnected');
 	});
 	socket.on('typing', function(user){
-		console.log('current', users_typing);
+		// console.log('current', users_typing);
 		if (users_typing.indexOf(user) < 0) {
-			console.log('adding user', user);
+			// console.log('adding user', user);
 			users_typing.push(user);
-			console.log('added', users_typing);
+			// console.log('added', users_typing);
 			io.emit('typing', users_typing);
 		}
 	});
+	socket.on('text_entered', function(user){
+		// console.log(user);
+		// console.log('text entered', users_text_entered);
+		// console.log(users_typing.indexOf(user));
+		if (users_text_entered.indexOf(user) < 0) {
+			// console.log('adding user to text enter', user);
+			users_text_entered.push(user);
+			// console.log('added to text enter', users_text_entered);
+			io.emit('text_entered', users_text_entered);
+		}
+	});
+	socket.on('text_entered_remove', function(user){
+		// console.log('text enter to remove', user, users_text_entered);
+		// console.log(users_text_entered.indexOf(user));
+		var index = users_text_entered.indexOf(user);
+		if (index >= 0) {
+			// console.log('removing user', user);
+			users_text_entered.splice(index, 1);
+			// console.log('removed', users_text_entered);
+			io.emit('text_entered', users_text_entered);
+		}
+	});
 	socket.on('typing_remove', function(user){
-		console.log('current -to remove', user, users_typing);
-		console.log(users_typing.indexOf(user));
+		// console.log('current -to remove', user, users_typing);
+		// console.log(users_typing.indexOf(user));
 		var index = users_typing.indexOf(user);
 		if (index >= 0) {
-			console.log('removing user', user);
+			// console.log('removing user', user);
 			users_typing.splice(index, 1);
-			console.log('removed', users_typing);
+			// console.log('removed', users_typing);
 			io.emit('typing', users_typing);
 		}
 	});
